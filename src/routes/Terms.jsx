264284fcf0769bbbeada24e9ce1b04ad35189d0e
Hasan-Mahadi <hasanmahadihm99@gmail.com>
2023-08-28 21:27:31 +0600

    Done by 80%

--- src/routes/Terms.jsx
+++ src/routes/Terms.jsx
@@ -3,10 +3,16 @@ import { Link } from 'react-router-dom';
 
 const Terms = () => {
     return (
-        <div>
-            <h2>Terms and Condition</h2>
+        <div className='p-20'>
+            <h2 className='text-purple-600 font-bold italic text-5xl text-center mb-5   hover:text-4xl transform hover:scale-110  transition delay-150 duration-300 ease-in-out '>Terms and Condition</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere itaque ipsum dolorum deserunt veritatis, molestias animi inventore culpa eligendi aperiam obcaecati harum distinctio dicta at ad possimus nihil doloremque labore.
             </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veniam voluptate officia quos ducimus maxime, ipsa possimus. Voluptatibus, nihil ea voluptates vitae officia labore vel similique quaerat pariatur enim corrupti!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veniam voluptate officia quos ducimus maxime, ipsa possimus. Voluptatibus, nihil ea voluptates vitae officia labore vel similique quaerat pariatur enim corrupti!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veniam voluptate officia quos ducimus maxime, ipsa possimus. Voluptatibus, nihil ea voluptates vitae officia labore vel similique quaerat pariatur enim corrupti!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veniam voluptate officia quos ducimus maxime, ipsa possimus. Voluptatibus, nihil ea voluptates vitae officia labore vel similique quaerat pariatur enim corrupti!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veniam voluptate officia quos ducimus maxime, ipsa possimus. Voluptatibus, nihil ea voluptates vitae officia labore vel similique quaerat pariatur enim corrupti!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veniam voluptate officia quos ducimus maxime, ipsa possimus. Voluptatibus, nihil ea voluptates vitae officia labore vel similique quaerat pariatur enim corrupti!</p>
 
             <div className='text-center mt-10  hover:text-4xl transform hover:scale-110  transition delay-150 duration-300 ease-in-out '>
                     <Link to='/register'> <button className="btn btn-primary  text-4xl ml-20"> Back to Register </button></Link>
